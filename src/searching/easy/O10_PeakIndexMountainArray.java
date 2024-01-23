c17ea913516a98e6bb7cd2090bf8b1955876a6f0
Anubhav Pandey <57632430+SpeedyAstro@users.noreply.github.com>
2024-01-23 14:40:37 +0530

    ` ✅   `

---
+++ src/searching/easy/O10_PeakIndexMountainArray.java
@@ -0,0 +1,16 @@
+package searching.easy;
+
+public class O10_PeakIndexMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0 , end = arr.length-1;
+        while(start<end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid]>arr[mid+1])
+                end = mid;
+            else
+                start = mid+1;
+        }
+        return start;
+    }
+}
+    
